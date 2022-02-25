f72cac9d882ba015123f622d8bf911c9af02e526
Fernandosantanasto <fernandosantana.sto@gmail.com>
2022-02-25 20:19:13 -0300

    projeto-blackjack

---
+++ modulo1/variaveis/index.js
@@ -0,0 +1,24 @@
+//Exercícios de interpretação de código
+
+//Exercício 1 
+
+//No exercício n°1 no primeiro console.log será impresso o valor 10, e no segundo console.log será impresso os valores 10 e 5
+
+
+
+
+
+
+
+// Exercícios de escrita de código
+// 1. 
+// let nome
+// let idade
+// console.log(typeof nome)
+// console.log(typeof idade)
+// //Não foi atribuído nenhum valor
+// nome = prompt('Qual é o seu nome?')
+// idade = prompt('Qual a sua idade?')
+// console.log(typeof nome)
+// console.log(typeof idade)
+// console.log (`Olá ${nome},  você tem ${idade} anos`)
