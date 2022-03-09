2a7126ec2e3e43e02707b419c24294c6b285b599
Fernandosantanasto <fernandosantana.sto@gmail.com>
2022-03-09 17:35:28 -0300

    variaveis

---
+++ modulo1/variaveis/template_javascript/template_javascript/index.js
@@ -0,0 +1,37 @@
+/*Exercícios de interpretação de código
+1 - Será impresso no primeiro console 10 e no segundo 10 e 5
+2- Será impresso 10,10,10
+
+3-
+let horasPorDiaTrabalhadas = prompt("Quantas horas você trabalha por dia?")
+let recebePorDia = prompt("Quanto você recebe por dia?")
+alert(`Voce recebe ${horasPorDiaTrabalhadas/recebePorDia} por hora`)
+
+Exercícios de escrita de código
+1-
+const nome = undefined 
+let idade = undefined
+console.log (typeof nome,idade)
+Foi impresso undefined porque não foi atribuído valor a variável
+
+const nome = "Fernando"; 
+const idade = "34";
+console.log (typeof nome,idade)
+A mensagem impressa foi string 34 o sistema reconheceu o nome e a idade
+
+const nome = prompt ("qual é o seu nome");
+const idade = prompt (qual é a sua idade);
+console.log ("Olá meu nome é", nome, "você tem 34 anos de idade");
+console.log (typeof nome,idade)/*
+
+2-
+
+const roupa = prompt ("Você está usando roupa azul?")
+console.log = (sim)
+
+3-
+
+let a = 10
+let b = 25
+console.log ("o valor de a é = b")
+console.log ("o valor de b é = a")*/
